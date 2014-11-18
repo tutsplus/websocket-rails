89f120bc88518f3a3a69fa671a02ee716b29c5cf
José Mota <jose@josemota.net>
2014-11-18 15:03:38 +0000

    Lesson 16.

--- lib/auction_socket.rb
+++ lib/auction_socket.rb
@@ -3,12 +3,14 @@ require File.expand_path "../place_bid", __FILE__
 class AuctionSocket
   def initialize app
     @app = app
+    @clients = []
   end
 
   def call env
     @env = env
     if socket_request?
       socket = spawn_socket
+      @clients << socket
       socket.rack_response
     else
       @app.call env
@@ -59,8 +61,15 @@ class AuctionSocket
 
     if service.execute
       socket.send "bidok"
+      notify_outbids socket, tokens[2]
     else
       socket.send "underbid #{service.auction.current_bid}"
+    end
+  end
+
+  def notify_outbids socket, value
+    @clients.reject { |client| client == socket }.each do |client|
+      client.send "outbid #{value}"
     end
   end
 end
